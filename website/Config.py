02b7fd6d76a99a24dad61de32d04ea6b5c0f8a07
AkilSheik <akil.sheik@gmail.com>
2021-09-02 15:09:52 +0000

    Added changes

--- website/Config.py
+++ website/Config.py
@@ -1,7 +1,17 @@
 import os
+import json
+
+with open('/etc/config.json') as config_file:
+    config = json.load(config_file)
+    print("made it")
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
+    SECRET_KEY = config.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
+    UPLOAD_FOLDER = config.get('UPLOAD_FOLDER')
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME  = config.get('EMAIL_USER')
+    MAIL_PASSWORD =  config.get('EMAIL_PASS')
     ALLOWED_FILE_EXTENSIONS = ["PNG", "JPG", "JPEG", "GIF", "PDF", "DOC", "TXT", "DOCX"]
