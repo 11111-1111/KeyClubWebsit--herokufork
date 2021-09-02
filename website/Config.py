62fdb4fe8cd9525803f2fc93a61e9e45b4758d0a
11111-1111 <87542547+11111-1111@users.noreply.github.com>
2021-09-02 10:48:01 -0400

    config mail
    flask mail config added

--- website/Config.py
+++ website/Config.py
@@ -1,7 +1,14 @@
 import os
+from flask_mail import Mail
+
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
     ALLOWED_FILE_EXTENSIONS = ["PNG", "JPG", "JPEG", "GIF", "PDF", "DOC", "TXT", "DOCX"]
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME  = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD =  os.environ.get('EMAIL_PASS')
