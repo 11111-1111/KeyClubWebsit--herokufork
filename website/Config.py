3280e922ed291dec47ec33f8f720ce6a890eeb92
Akil <akil.sheik@gmail.com>
2021-09-14 10:40:48 +0000

    Commited

--- website/Config.py
+++ website/Config.py
@@ -1,9 +1,13 @@
 import os
+
 import json
 
 with open('/etc/config.json') as config_file:
     config = json.load(config_file)
     print("made it")
+
+from flask_mail import Mail
+
 
 class Config:
     SECRET_KEY = config.get('SECRET_KEY')
@@ -12,6 +16,7 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME  = config.get('EMAIL_USER')
-    MAIL_PASSWORD =  config.get('EMAIL_PASS')
+    MAIL_USERNAME  = config.get('MAIL_USERNAME')
+    MAIL_PASSWORD =  config.get('MAIL_PASSWORD')
+    ADMIN_PASSWORD = config.get('ADMIN_PASSWORD')
     ALLOWED_FILE_EXTENSIONS = ["PNG", "JPG", "JPEG", "GIF", "PDF", "DOC", "TXT", "DOCX"]
