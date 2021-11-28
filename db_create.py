db273b957bc3496687d2146e381db49185486a0c
11111-1111 <87542547+11111-1111@users.noreply.github.com>
2021-11-28 12:23:50 -0500

    comments on database creation and deletion

--- db_create.py
+++ db_create.py
@@ -1,3 +1,10 @@
+#to destroy the database:
+# 1. cd to github folder, and run: heroku git:remote -a key-club
+# 2. run heroku pg:reset DATABASE
+
+
+#the code below creates the postgres database. To run it, go to heroku console and run: python db_create.py
+
 from __init__ import db
 from views import app
 db.init_app(app)
