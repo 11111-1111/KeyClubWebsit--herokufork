22562cd0945a12be0acdfa3b1bdb90826800ece4
11111-1111 <87542547+11111-1111@users.noreply.github.com>
2021-11-28 00:42:13 -0500

    heroku is outdated

--- website/Config.py
+++ website/Config.py
@@ -10,6 +10,8 @@ from flask_mail import Mail
 class Config:
        SECRET_KEY = os.environ.get('SECRET_KEY')
        SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+       if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+              SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
        UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
        MAIL_SERVER = 'smtp.googlemail.com'
        MAIL_PORT = 587
