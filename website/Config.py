efb137bf5e1abc887df0b347af3f307cf15d6617
11111-1111 <87542547+11111-1111@users.noreply.github.com>
2021-11-27 22:55:04 -0500

    changed database

--- website/Config.py
+++ website/Config.py
@@ -15,7 +15,7 @@ from flask_mail import Mail
 class Config:
     if(platform == "linux"):
         SECRET_KEY = config.get('SECRET_KEY')
-        SQLALCHEMY_DATABASE_URI = config.get('SQLALCHEMY_DATABASE_URI')
+        SQLALCHEMY_DATABASE_URI = config.get('DATABASE_URL')
         UPLOAD_FOLDER = config.get('UPLOAD_FOLDER')
         MAIL_SERVER = 'smtp.googlemail.com'
         MAIL_PORT = 587
