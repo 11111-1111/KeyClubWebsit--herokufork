65544ffb9d24322dec939c3c99f927298705d345
11111-1111 <87542547+11111-1111@users.noreply.github.com>
2021-11-28 00:24:51 -0500

    no config if

--- website/Config.py
+++ website/Config.py
@@ -8,25 +8,13 @@ from flask import app
 from flask_mail import Mail
 
 class Config:
-    if(platform == "linux"):
-        SECRET_KEY = config.get('SECRET_KEY')
-        SQLALCHEMY_DATABASE_URI = config.get('DATABASE_URL')
-        UPLOAD_FOLDER = config.get('UPLOAD_FOLDER')
-        MAIL_SERVER = 'smtp.googlemail.com'
-        MAIL_PORT = 587
-        MAIL_USE_TLS = True
-        MAIL_USERNAME  = config.get('MAIL_USERNAME')
-        MAIL_PASSWORD =  config.get('MAIL_PASSWORD')
-        ADMIN_PASSWORD = config.get('ADMIN_PASSWORD')
-        ALLOWED_FILE_EXTENSIONS = ["PNG", "JPG", "JPEG", "GIF", "PDF", "DOC", "TXT", "DOCX"]
-    elif(platform == "win32"):
-        SECRET_KEY = os.environ.get('SECRET_KEY')
-        SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-        UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
-        MAIL_SERVER = 'smtp.googlemail.com'
-        MAIL_PORT = 587
-        MAIL_USE_TLS = True
-        MAIL_USERNAME  = os.environ.get('MAIL_USERNAME')
-        MAIL_PASSWORD =  os.environ.get('MAIL_PASSWORD')
-        ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
-        ALLOWED_FILE_EXTENSIONS = ["PNG", "JPG", "JPEG", "GIF", "PDF", "DOC", "TXT", "DOCX"]
+       SECRET_KEY = os.environ.get('SECRET_KEY')
+       SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+       UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
+       MAIL_SERVER = 'smtp.googlemail.com'
+       MAIL_PORT = 587
+       MAIL_USE_TLS = True
+       MAIL_USERNAME  = os.environ.get('MAIL_USERNAME')
+       MAIL_PASSWORD =  os.environ.get('MAIL_PASSWORD')
+       ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
+       ALLOWED_FILE_EXTENSIONS = ["PNG", "JPG", "JPEG", "GIF", "PDF", "DOC", "TXT", "DOCX"]
