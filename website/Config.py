c434e25c4f7573234fb295c38a118ecec0cf1de0
11111-1111 <87542547+11111-1111@users.noreply.github.com>
2021-11-28 00:32:23 -0500

    url not uri

--- website/Config.py
+++ website/Config.py
@@ -9,7 +9,7 @@ from flask_mail import Mail
 
 class Config:
        SECRET_KEY = os.environ.get('SECRET_KEY')
-       SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+       SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
        UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
        MAIL_SERVER = 'smtp.googlemail.com'
        MAIL_PORT = 587
